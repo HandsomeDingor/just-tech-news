9bcb7f976abf0e33888b2b18f39f0c3b2b29206b
HandsomeDingor <jay807541931@gmail.com>
2022-03-15 03:13:36 -0400

    add

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
 DROP DATABASE IF EXISTS just_tech_news_db;
 
 CREATE DATABASE just_tech_news_db;
+
+-- use just_tech_news_db;
+
+-- INSERT INTO post (title, post_url, user_id, created_at, updated_at)
+-- VALUES ("Taskmaster goes public!", "https://taskmaster/press", 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
